880c19b537acfa871c98ee8ca4d097c88ea3f4fc
Devina.Kumar.18@dartmouth.edu <devinakumar@devinas-air-2.wireless.dartmouth.edu>
2016-08-10 17:59:07 -0400

    backend

---
+++ app/controllers/user_controller.js
@@ -0,0 +1,38 @@
+import jwt from 'jwt-simple';
+import User from '../models/user_model';
+import config from '../config.js';
+// import * as users from '../models/user_model';
+
+export const signin = (req, res, next) => {
+  res.send({ token: tokenForUser(req.user) });
+};
+
+export const signup = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  if (!email || !password) {
+    return res.status(422).send('You must provide email and password');
+  }
+  User.findOne({ email })
+  .then(result => {
+    if (result) {
+      return res.status(422).send('This user already exists');
+    } else {
+      const user = new User();
+      user.email = email;
+      user.password = password;
+      user.save();
+      return res.send({ token: tokenForUser(user) });
+    }
+  }).catch(error => {
+    console.log(error);
+    return res.send(`errror ${error}`);
+  });
+};
+
+// encodes a new token for a user object
+function tokenForUser(user) {
+  const timestamp = new Date().getTime();
+  return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+}
